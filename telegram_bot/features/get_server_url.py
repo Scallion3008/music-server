96d4d088521201d9c4ebe49ceb5200c070402fb9
Ada Ho <25481871+Scallion3008@users.noreply.github.com>
2026-02-13 22:54:28 +0800

    Update server URL message

--- telegram_bot/features/get_server_url.py
+++ telegram_bot/features/get_server_url.py
@@ -1,20 +1,25 @@
 import requests
 import logging
+import socket
 from urllib.parse import urlparse
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 help_str = "/server - View SSH server address and port"
 
 async def get_server_addr(update: Update, context: ContextTypes.DEFAULT_TYPE):
   try:
-    tunnels = requests.get("http://localhost:4040/api/tunnels").json()["tunnels"]
-    tunnel_url = urlparse(tunnels[0]["public_url"]).netloc
+    hostname = socket.gethostname()
   except Exception as ex:
     logging.error(f"Error retrieving SSH server address: {ex}")
     return await update.message.reply_text("Error retrieving SSH server address.")
 
-  await update.message.reply_text(f"SSH server address: {tunnel_url}")
+  await update.message.reply_text(
+    f"SSH server address: `{hostname}:2222`\n"
+    "Remember to connect to your Tailnet before SSHing in or running rsync\!",
+    parse_mode=ParseMode.MARKDOWN_V2,
+  )
 
 def add_handlers(application: Application):
   application.add_handler(CommandHandler("server", get_server_addr))
